df9b3aa3ebcf3b85d44b4218282eea23be4657f9
vipasha <vips3201v@gmail.com>
2020-09-19 23:58:41 +0530

    Updates

---
+++ precache-manifest.559e6f037b0b423473f6fe37be27685f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66354bf84f5e792d3eb93549569c02a",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/index.html"
+  },
+  {
+    "revision": "10b0e2c64a8d4dfc9ab4",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "cd7459f548769b0c6ac6",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/css/main.4dfe1294.chunk.css"
+  },
+  {
+    "revision": "10b0e2c64a8d4dfc9ab4",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/js/2.998b53e4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/js/2.998b53e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7459f548769b0c6ac6",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/js/main.31afe92c.chunk.js"
+  },
+  {
+    "revision": "e958351cb9209089f4d0",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/js/runtime-main.ffb5aa1f.js"
+  },
+  {
+    "revision": "d5b57417a8277b4f4b180191c3d35981",
+    "url": "/vipashaaV321.github.io/Crypto-Tracker/static/media/1.d5b57417.jpg"
+  }
+]);
